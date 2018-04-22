b7f74359b453f05f2cf479d458d85a830654c13a
Lidiacon <lidiacon@ucm.es>
2018-04-22 18:37:39 +0200

    Añadida clase Enemy

--- practica3.js
+++ practica3.js
@@ -3,7 +3,9 @@ window.addEventListener("load",function() {
                 .setup({height: 480, width: 320})
                 .controls().touch();
 
-
+//
+//  PLAYER
+//
   Q.Sprite.extend("Player", {
     init:function(p){
       this._super(p, {
@@ -12,31 +14,90 @@ window.addEventListener("load",function() {
         y:380
       });
       this.add('2d, platformerControls');
+      this.on("enemy.hit","enemyHit");
+
     },
 
     reset:function(){
       Q.stageScene("level1");
+      //this.p.x = 150;
+      //this.p.y = 380;
     },
 
     step:function(dt){
-      super.step(dt);
-      if(this.p.y < 0){
+      //this.super.step(dt);
+      if(this.p.y > 700){
         this.reset();
       }
+    },
+
+    enemyHit:function(){
+      this.reset();
     }
   });
 
-  Q.scene("level1",function(stage) {
-    Q.stageTMX("level.tmx",stage);
-    //stage.collisionLayer()
-    var player = stage.insert(new Q.Player());
-    stage.add("viewport").follow(player, {x:true, y:true}, {minX:0, minY:0, maxY:600});
-    //.centerOn(150, 380);
-    //stage.add("Player");
+//
+//  GOOMBA
+//
+  Q.Sprite.extend("Enemy", {
+    init:function(p){
+      this._super(p,{
+        vx:50,
+        defaultDirection:'left'
+      });
+      this.add("2d, aiBounce");
+      this.on("bump.top",this, function(collision){
+        if(collision.obj.isA("Player")){
+          this.p.dead = true;
+        }
+      });
+
+      this.on("bump.left,bump.right", this, "hit");
+    },
+
+    hit:function(col){
+      if(col.obj.isA("Player")){
+          col.obj.trigger("enemy.hit");
+      }
+    }
+  })
+
+  Q.Enemy.extend("Goomba", {
+    init: function(p) {
+      this._super(p,{
+        sheet:"goomba",
+        x:500,
+        y:380
+      })
+    }
+  });
+  Q.Enemy.extend("Bloopa", {
+    init: function(p) {
+      this._super(p,{
+        sheet:"bloopa",
+        x:500,
+        y:300
+      })
+    }
   });
 
-  Q.load("mario_small.json, mario_small.png", function(){
+//
+//  SCENE
+//
+  Q.scene("level1",function(stage) {
+    Q.stageTMX("level.tmx",stage);
+    var player = stage.insert(new Q.Player());
+    stage.add("viewport").follow(player, {x:true, y:true}, {minX:0, minY:0, maxY:600});
+    stage.insert(new Q.Enemy({sheet:"goomba",x:500,y:380}));
+    stage.insert(new Q.Enemy({sheet:"bloopa",x:700,y:400}));
+
+  });
+
+  Q.load("mario_small.json, mario_small.png, goomba.json, goomba.png, bloopa.json, bloopa.png", function(){
     Q.compileSheets("mario_small.png", "mario_small.json");
+    Q.compileSheets("goomba.png", "goomba.json");
+    Q.compileSheets("bloopa.png", "bloopa.json");
+
     Q.loadTMX("level.tmx", function() {
       //Q.compileSheets("sprites.png","sprites.json");
      
